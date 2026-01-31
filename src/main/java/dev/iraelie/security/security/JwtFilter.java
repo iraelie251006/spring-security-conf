3b5869aa1c52e4cbb6f07c07113c92b4147bcd3c
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-31 23:54:18 +0200

    Added missing dependencies and imports.

--- src/main/java/dev/iraelie/security/security/JwtFilter.java
+++ src/main/java/dev/iraelie/security/security/JwtFilter.java
@@ -1,9 +1,16 @@
 package dev.iraelie.security.security;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@Service
 public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(
