9cd598b895ec185694c5bd6ea8bd51574b4fc5b9
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-31 23:59:36 +0200

    Created User class

--- src/main/java/dev/iraelie/security/user/User.java
+++ src/main/java/dev/iraelie/security/user/User.java
@@ -1,4 +1,20 @@
 package dev.iraelie.security.user;
 
 public class User {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private LocalDate dateOfBirth;
+    private boolean enabled;
+    private boolean locked;
+    private boolean credentialsExpired;
+    private boolean emailVerified;
+    private String profilePictureUrl;
+    private boolean phoneVerified;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
 }
