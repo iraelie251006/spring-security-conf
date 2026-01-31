b1edaa564700ff7531366223d2ca2deeeb282c49
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-31 23:31:20 +0200

    Disabled CSRF and implemented methods for OncePerRequestFilter.

--- src/main/java/dev/iraelie/security/security/JwtFilter.java
+++ src/main/java/dev/iraelie/security/security/JwtFilter.java
@@ -2,5 +2,14 @@ package dev.iraelie.security.security;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
 public class JwtFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
--- src/main/java/dev/iraelie/security/security/SecurityConfig.java
+++ src/main/java/dev/iraelie/security/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -38,6 +39,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth.requestMatchers(PUBLIC_URLS)
                                 .permitAll()
