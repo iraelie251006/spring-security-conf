6eccc1b0fd01e2ee2a3744b98722026d26cbe0f9
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-01 23:48:07 +0200

    fix @NullMarked error by using @NonNull

--- src/main/java/dev/iraelie/security/user/User.java
+++ src/main/java/dev/iraelie/security/user/User.java
@@ -57,6 +57,7 @@ public class User implements UserDetails {
     private LocalDateTime lastModifiedDate;
 
     @Override
+    @NonNull
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
     }
