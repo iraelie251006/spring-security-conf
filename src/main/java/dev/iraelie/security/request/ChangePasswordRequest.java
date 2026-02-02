37b4c0f4877bc727550a16dd16b7c5550cc7a4e7
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-02 23:43:39 +0200

    Created dto to change password

---
+++ src/main/java/dev/iraelie/security/request/ChangePasswordRequest.java
@@ -0,0 +1,19 @@
+package dev.iraelie.security.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChangePasswordRequest {
+    private String currentPassword;
+    private String newPassword;
+    private String confirmNewPassword;
+
+}
