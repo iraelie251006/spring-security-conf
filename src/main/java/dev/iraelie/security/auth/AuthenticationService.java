e0773e993d072d8f134dd0a5dffff986393fb671
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-05 23:41:08 +0200

    Finished implementation of AuthenticationService interface

--- src/main/java/dev/iraelie/security/auth/AuthenticationService.java
+++ src/main/java/dev/iraelie/security/auth/AuthenticationService.java
@@ -1,4 +1,15 @@
 package dev.iraelie.security.auth;
 
-public class AuthenticationService {
+import dev.iraelie.security.auth.request.AuthenticationRequest;
+import dev.iraelie.security.auth.request.RefreshRequest;
+import dev.iraelie.security.auth.response.AuthenticationResponse;
+import dev.iraelie.security.request.RegistrationRequest;
+
+public interface AuthenticationService {
+
+    AuthenticationResponse login(AuthenticationRequest request);
+
+    void register(RegistrationRequest request);
+
+    AuthenticationResponse refreshToken(RefreshRequest req);
 }
