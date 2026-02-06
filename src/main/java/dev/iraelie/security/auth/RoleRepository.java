b458f0de5e0c8fc0f52566a996d6d063d90dda07
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-06 19:24:19 +0200

    Implemented RoleRepository

---
+++ src/main/java/dev/iraelie/security/auth/RoleRepository.java
@@ -0,0 +1,12 @@
+package dev.iraelie.security.auth;
+
+
+import dev.iraelie.security.role.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, String> {
+
+    Optional<Role> findByName(String roleUser);
+}
