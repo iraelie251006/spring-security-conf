b1618d2578c7b106e6744e4679fd1114e2c9b6a7
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-06 21:24:19 +0200

    Marked register transactional to rollback once one part fails.

--- src/main/java/dev/iraelie/security/auth/AuthenticationServiceImpl.java
+++ src/main/java/dev/iraelie/security/auth/AuthenticationServiceImpl.java
@@ -23,6 +23,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -67,6 +68,7 @@ public class AuthenticationServiceImpl implements AuthenticationService{
     }
 
     @Override
+    @Transactional
     public void register(RegistrationRequest request) {
         checkUserEmail(request.getEmail());
         checkUserPhoneNumber(request.getPhoneNumber());
