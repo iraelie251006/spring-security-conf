4e754357e5b91db320559a8ab9bd6be881d18237
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-06 19:25:13 +0200

    Implemented Authentication dtos response and requests.

--- src/main/java/dev/iraelie/security/auth/request/RefreshRequest.java
+++ src/main/java/dev/iraelie/security/auth/request/RefreshRequest.java
@@ -1,4 +1,16 @@
 package dev.iraelie.security.auth.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RefreshRequest {
+    private String refreshToken;
 }
