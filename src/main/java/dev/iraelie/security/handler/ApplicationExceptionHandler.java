f1248d9b97bde3b60144b94d32f583f004c951b9
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-07 23:34:00 +0200

    Handled Disabled exception error

--- src/main/java/dev/iraelie/security/handler/ApplicationExceptionHandler.java
+++ src/main/java/dev/iraelie/security/handler/ApplicationExceptionHandler.java
@@ -4,15 +4,19 @@ import dev.iraelie.security.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static dev.iraelie.security.exception.ErrorCode.ERR_USER_DISABLED;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @RestControllerAdvice
 @Slf4j
 @RequiredArgsConstructor
 public class ApplicationExceptionHandler {
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusiness(final BusinessException ex) {
         final ErrorResponse body = ErrorResponse.builder()
@@ -25,6 +29,16 @@ public class ApplicationExceptionHandler {
         return ResponseEntity.status(ex.getErrorCode()
                         .getStatus() != null ? ex.getErrorCode()
                         .getStatus() : BAD_REQUEST)
+                .body(body);
+    }
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<ErrorResponse> handleBusiness() {
+        final ErrorResponse body = ErrorResponse.builder()
+                .code(ERR_USER_DISABLED.getCode())
+                .message(ERR_USER_DISABLED.getDefaultMessage())
+                .build();
+        return ResponseEntity.status(UNAUTHORIZED)
                 .body(body);
     }
 }
