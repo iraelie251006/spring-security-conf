c545ad987d81168e55f843507f334e717f6a97b8
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-07 23:32:29 +0200

    Handler Business exception error

--- src/main/java/dev/iraelie/security/handler/ApplicationExceptionHandler.java
+++ src/main/java/dev/iraelie/security/handler/ApplicationExceptionHandler.java
@@ -1,4 +1,30 @@
 package dev.iraelie.security.handler;
 
+import dev.iraelie.security.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice
+@Slf4j
+@RequiredArgsConstructor
 public class ApplicationExceptionHandler {
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusiness(final BusinessException ex) {
+        final ErrorResponse body = ErrorResponse.builder()
+                .code(ex.getErrorCode()
+                        .getCode())
+                .message(ex.getMessage())
+                .build();
+        log.info("Business Exception: {}", body);
+        log.debug(ex.getMessage(), ex);
+        return ResponseEntity.status(ex.getErrorCode()
+                        .getStatus() != null ? ex.getErrorCode()
+                        .getStatus() : BAD_REQUEST)
+                .body(body);
+    }
 }
