38c9619567aa5dc53413a6c81237477ff8c0f022
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-07 23:35:21 +0200

    Handled Method argument not valid exception error

--- src/main/java/dev/iraelie/security/handler/ApplicationExceptionHandler.java
+++ src/main/java/dev/iraelie/security/handler/ApplicationExceptionHandler.java
@@ -5,8 +5,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static dev.iraelie.security.exception.ErrorCode.ERR_USER_DISABLED;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -40,5 +45,25 @@ public class ApplicationExceptionHandler {
                 .build();
         return ResponseEntity.status(UNAUTHORIZED)
                 .body(body);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleException(final MethodArgumentNotValidException exp) {
+        final List<ErrorResponse.ValidationError> errors = new ArrayList<>();
+        exp.getBindingResult()
+                .getAllErrors()
+                .forEach(error -> {
+                    final String fieldName = ((FieldError) error).getField();
+                    final String errorCode = error.getDefaultMessage();
+                    errors.add(ErrorResponse.ValidationError.builder()
+                            .field(fieldName)
+                            .code(errorCode)
+                            .message(errorCode)
+                            .build());
+                });
+        final ErrorResponse errorResponse = ErrorResponse.builder()
+                .validationErrors(errors)
+                .build();
+        return new ResponseEntity<>(errorResponse, BAD_REQUEST);
     }
 }
