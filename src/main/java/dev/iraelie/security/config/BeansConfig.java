49008076ea076780b9781e4732172defdb9ad939
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-10 10:49:06 +0200

    Implemented authentication manager & auditor aware beans

--- src/main/java/dev/iraelie/security/config/BeansConfig.java
+++ src/main/java/dev/iraelie/security/config/BeansConfig.java
@@ -2,6 +2,9 @@ package dev.iraelie.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,5 +13,15 @@ public class BeansConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new ApplicationAuditorAware();
     }
 }
