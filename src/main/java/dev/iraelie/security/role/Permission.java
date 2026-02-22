8102ba52a448e060b83f9a18e5e1f2d8fd8bb9ec
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-22 23:53:53 +0200

    Added blog permissions

--- src/main/java/dev/iraelie/security/role/Permission.java
+++ src/main/java/dev/iraelie/security/role/Permission.java
@@ -4,6 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum Permission {
+    BLOG_READ("blog:read"),               // Anyone can read blogs
+    BLOG_CREATE("blog:create"),           // Can write new blog posts
+    BLOG_EDIT_OWN("blog:edit:own"),       // Can edit ONLY their own posts
+    BLOG_EDIT_ANY("blog:edit:any"),       // Can edit ANY author's post
+    BLOG_DELETE_OWN("blog:delete:own"),   // Can delete ONLY their own posts
+    BLOG_DELETE_ANY("blog:delete:any"),   // Can delete ANY post
+    BLOG_PUBLISH("blog:publish");
+
     private final String permission;
 
     Permission(String permission) {
