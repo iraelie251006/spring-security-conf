adf93d2361d12a50a98bfa250243db3f4069811a
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-23 23:33:14 +0200

    added space 2

--- src/main/java/dev/iraelie/security/role/Permission.java
+++ src/main/java/dev/iraelie/security/role/Permission.java
@@ -13,10 +13,13 @@ public enum Permission {
     BLOG_DELETE_ANY("blog:delete:any"),   // Can delete ANY post
     BLOG_PUBLISH("blog:publish"),
 
+
     // COMMENT PERMISSIONS
     COMMENT_READ("comment:read"),
     COMMENT_WRITE("comment:write"),
     COMMENT_MANAGE("comment:manage"),     // Can delete/hide any comment
+
+
 
     // USER MANAGEMENT PERMISSIONS (Admin only)
     USER_MANAGE("user:manage"),           // Can manage all users
